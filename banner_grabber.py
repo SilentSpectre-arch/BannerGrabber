5ffe161d40f488f6a95c5b513965b6f8124e957f
SilentSpectre-arch <amirootrpr234@gmail.com>
2026-08-15 21:06:53 +0330

    build a simple scanner for now

---
+++ banner_grabber.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import socket
+
+target=input("Enter target: ")
+
+port=input("Enter port: ")
+
+sock= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sock.settimeout(3)
+
+result=sock.connect_ex((target, port))
+
+if result == 0:
+    print(f"[+] {target}:{port} is open")
+else:
+    print(f'[-] {target}:{port} is closed')
+
+sock.close()
