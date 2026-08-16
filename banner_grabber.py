233b5c05e24a81dd730e1c02db3e5ca17dd79782
SilentSpectre-arch <amirootrpr234@gmail.com>
2026-08-16 18:22:52 +0330

    refactor: give banner and add try and exception

--- banner_grabber.py
+++ banner_grabber.py
@@ -10,11 +10,24 @@ sock= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 sock.settimeout(3)
 
-result=sock.connect_ex((target, port))
+try:
 
-if result == 0:
-    print(f"[+] {target}:{port} is open")
-else:
-    print(f'[-] {target}:{port} is closed')
+    result=sock.connect((target,port))
 
-sock.close()
+    print(f'[+] Connected to {target}:{port}')
+
+    banner=sock.recv(1024)
+
+    print(f'[+] Banner: {banner.decode(errors='ignore').strip()}')
+
+except socket.timeout:
+    print("[-] Connection timed out")
+
+except ConnectionRefusedError:
+    print(f"[-] Connection refused")
+
+except Exception as e:
+    print(f'[-] Error: {e}')
+
+finally:
+    sock.close()
