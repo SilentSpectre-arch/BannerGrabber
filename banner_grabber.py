b8bf70cfac31eaeffa206f76055c099dadec2942
SilentSpectre-arch <amirootrpr234@gmail.com>
2026-08-16 19:15:56 +0330

    refactor script add http grab banner and send http request

--- banner_grabber.py
+++ banner_grabber.py
@@ -31,9 +31,44 @@ def grab_banner(target,port):
     finally:
         sock.close()
 
+def grab_http_banner(target,port):
+    sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    sock.settimeout(3)
+
+    try:
+        sock.connect((target,port))
+
+        request = (
+            f'HEAD / HTTP/1.1\r\n'
+            f'Host: {target}\r\n'
+            f'Connection: close\r\n'
+            f'\r\n'
+        )
+
+        sock.sendall(request.encode())
+
+        response=sock.recv(4096)
+
+        print(f'[+] {target}:{port} HTTP OPEN')
+        print(response.decode(errors='ignore'))
+
+    except socket.timeout:
+        print(f"[-] {target}:{port} TIMEOUT")
+
+    except ConnectionRefusedError:
+        print(f'[-] {target}:{port} CLOSED')
+
+    except Exception as e:
+        print(f'[-] {target}:{port} ERROR: {e}')
+
+    finally:
+        sock.close()
+
 target = input("Enter target: ")
 
 ports = [21,22,23,25,53,80,110,143,443,3306,8080]
 
-for port in ports:
-    grab_banner(target,port)
+# for port in ports:
+#     grab_banner(target,port)
+
+grab_http_banner(target, 8000)
